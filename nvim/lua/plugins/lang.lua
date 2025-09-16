e42585a00a29f0b86bc740ccb7d44d3700136328
vanppo <vanppo@hotmail.com>
2025-09-16 22:13:15 +0800

    :tada: Initial commit

---
+++ nvim/lua/plugins/lang.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          elixirls = { enabled = false },
+        },
+      },
+    },
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "lexical")
+      end,
+    },
+    {
+      "elixir-editors/vim-elixir",
+      ft = "elixir",
+    },
+  },
+}
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = { style = "float" },
+    },
+  },
+}
